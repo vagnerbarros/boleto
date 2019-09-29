d8196049471f83143c9aa0fc25beab6069a46f07
Vagner Barros <vagnerbarrospereira@gmail.com>
2019-09-29 20:22:39 -0300

    Campos de boleto

--- schemas/Boleto.js
+++ schemas/Boleto.js
@@ -2,20 +2,29 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var boletoSchema = new Schema({
+  banco: { type: String, required: true },
+  linha_digitavel: { type: String, required: true },
+  agencia_cedente: { type: String, required: true },
+  nosso_numero: { type: String, required: true },
+  codigo_barras: { type: String, required: true },
   carteira: { type: String, required: true },
   descricao: { type: String, required: true },
   valor: { type: Number, required: true },
   desconto: { type: Number },
   vencimento: { type: Date, required: true },
   emissao: { type: Date, required: true, default: Date.now() },
-  destinatario: {
+  instrucao: { type: String },
+
+  sacado: {
     nome: { type: String, required: true},
     cnpj_cpf: { type: String, required: true},
     telefone: { type: String, required: true },
     email: { type: String },
   },
-  emissor: {
-    
+  cedente: {
+    nome: { type: String, required: true },
+    cnpj_cpf: { type: String, required: true },
+    tipo: { type: String, required: true },
   }
 });
 
