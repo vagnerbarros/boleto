7ee17ef5544d1f0bfe263d5c44e7ab5ca567a2a4
Vagner Barros <vagnerbarrospereira@gmail.com>
2019-09-29 19:49:02 -0300

    Dados do Boleto

--- schemas/Boleto.js
+++ schemas/Boleto.js
@@ -2,7 +2,21 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var boletoSchema = new Schema({
-  banco: String,
+  carteira: { type: String, required: true },
+  descricao: { type: String, required: true },
+  valor: { type: Number, required: true },
+  desconto: { type: Number },
+  vencimento: { type: Date, required: true },
+  emissao: { type: Date, required: true, default: Date.now() },
+  destinatario: {
+    nome: { type: String, required: true},
+    cnpj_cpf: { type: String, required: true},
+    telefone: { type: String, required: true },
+    email: { type: String },
+  },
+  emissor: {
+    
+  }
 });
 
 var Boleto = mongoose.model("boleto", boletoSchema);
